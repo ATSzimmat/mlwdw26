5d7946522199efc28e9ba68506feadfde35806af
ATSzimmat <anton.szimmat@campus.lmu.de>
2026-02-14 11:04:01 +0100

    push 40

--- R/create_df_2.R
+++ R/create_df_2.R
@@ -1,6 +1,6 @@
 #' Create an evidence dataset from the .txt-files that is ready for use with merge_df
 #'
-#' This function creates a DataFrame from all .txt-files located in the specified folder, containing the columns "langer_beleg", "pruef_beleg" and "pruef_stelle". The files used in the example were automatically loaded with the package and should be located in your project folder as a folder named "toy", after you installed the package.
+#' This function creates a DataFrame from all .txt-files located in the specified folder, containing the columns "langer_beleg", "pruef_beleg" and "pruef_stelle". The files used in the example were automatically loaded with the package and should be located in your project folder as a folder named "toy", after you installed the package and executed convert_doc on the doc_folder.
 #'
 #' @param txt_folder Path to a certain folder containing .txt-files.
 #' @returns An evidence dataset containing the columns "langer_beleg", "pruef_beleg" and "pruef_stelle" and that is ready for use with merge_df
